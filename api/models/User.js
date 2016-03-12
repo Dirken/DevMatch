83b53a29dcf534308a79b22b06a2816db548bee4
cristianpinto <cristianpintoquinto@gmail.com>
2016-03-12 19:28:37 +0100

    user signup

--- api/models/User.js
+++ api/models/User.js
@@ -4,14 +4,14 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
   attributes: {
 
     username: {
-      type: 'string',
-      primaryKey: true
+      type: 'string'
     },
     password: {
       type: 'string',
@@ -27,7 +27,8 @@ module.exports = {
     	type: 'integer'
     },
     email: {
-    	type: 'email'
+    	type: 'email',
+        primaryKey: true
     },
     phone:{
     	type: 'integer'
