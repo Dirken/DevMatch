188154d3b56b66d31afbbc76475836626e77dd1a
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2016-03-12 14:47:54 +0100

    relationships test2

--- api/controllers/RelationshipsController.js
+++ api/controllers/RelationshipsController.js
@@ -7,22 +7,8 @@
 
 module.exports = {
 	relationships: function (req, res) {
-    	var username = req.param("username");
-    	user.findByUsername(username).done(function(err,usr){
-    		if (err){
-    			res.send(500, { error: "DB Error" });
-    		}
-    		else if (!usr){
-    			res.send(400,{ error: "Not user in DB"});
-    		}
-    		else {
-    			Pet.query('SELECT DISTINCT u1.username FROM User u1 WHERE u1.events in (SELECT u2.events FROM User u2)', function(err, results) {
-  				if (err) return res.serverError(err);
-  				return res.ok(results.rows);
-				});
-        	}
-        });
+        var myQuery = User.find();
+        myQuery.where({'name':{startsWith:'J'}});
 	}
-	
 };
 
